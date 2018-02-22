534b5c82c68060eb927cbf3f161970eabae0742a
Robert Weclawski <robert.weclawski@mykolab.com>
2018-02-22 23:15:38 +0100

    noop

--- main.go
+++ main.go
@@ -14,11 +14,7 @@ func main() {
 	// Set Gin to production mode
 	gin.SetMode(gin.ReleaseMode)
 
-	// Set the router as the default one provided by Gin
 	router = gin.Default()
-
-	// Process the templates at the start so that they don't have to be loaded
-	// from the disk again. This makes serving HTML pages very fast.
 	router.LoadHTMLGlob("templates/*")
 
 	// Initialize the routes
