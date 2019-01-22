58073fb11fc1342e1592352e1555400c1bbc0b07
Moritz Eysholdt <moritz.eysholdt@typefox.io>
2019-01-22 12:49:59 +0000

    Added Article

--- models.article.go
+++ models.article.go
@@ -16,6 +16,7 @@ type article struct {
 var articleList = []article{
 	article{ID: 1, Title: "Article 1", Content: "Article 1 body"},
 	article{ID: 2, Title: "Article 2", Content: "Article 2 body"},
+    article{ID: 2, Title: "Article 3", Content: "Article 3 body"},
 }
 
 // Return a list of all the articles
